563520f82fd5939a2b60a2ceb7b4f7fa40bd4002
albanmahmut <albanmahmut@gmail.com>
2024-05-04 22:47:13 +0200

    added base url and api key

--- src/main/java/com/weathercom/weatherapi/config.java
+++ src/main/java/com/weathercom/weatherapi/config.java
@@ -1,4 +1,15 @@
 package com.weathercom.weatherapi;
 
 public class config {
+    private static final String BASE_URL = "";
+    private static final String API_KEY = "";
+
+    public static String getBaseUrl() {
+        return BASE_URL;
+    }
+
+    public static String getApiKey() {
+        return API_KEY;
+    }
+
 }
