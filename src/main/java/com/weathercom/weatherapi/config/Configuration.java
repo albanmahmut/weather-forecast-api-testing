728e2e4cd9217cc320d35a4b56307e426c5c9662
albanmahmut <albanmahmut@gmail.com>
2024-05-04 23:04:50 +0200

    model class for data structures

--- src/main/java/com/weathercom/weatherapi/config/Configuration.java
+++ src/main/java/com/weathercom/weatherapi/config/Configuration.java
@@ -1,6 +1,6 @@
-package com.weathercom.weatherapi;
+package com.weathercom.weatherapi.config;
 
-public class config {
+public class Configuration {
     private static final String BASE_URL = "";
     private static final String API_KEY = "";
 
---
+++ src/main/java/com/weathercom/weatherapi/model/WeatherData.java
@@ -0,0 +1,4 @@
+package com.weathercom.weatherapi.model;
+
+public class WeatherData {
+}
